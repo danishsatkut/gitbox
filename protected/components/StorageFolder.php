077424b6a997af76e810a18b3628916954570c88
Danish Satkut <danish_satkut@hotmail.com>
2012-04-09 02:51:07 +0530

    Added the StorageFolder class.

---
+++ protected/components/StorageFolder.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Description of StorageFolder
+ *
+ * @author Danish Satkut
+ */
+class StorageFolder extends CApplicationComponent {
+    private $_path = "C:\xampp\htdocs\working-sandbox\gitbox\protected\storage";
+    private $_folder;
+    
+    public function __construct() {
+        $this->_folder = CFile::set($this->_path);
+    }
+    
+    public function getFolder() {
+        return $this->_folder;
+    }
+}
+
+?>
