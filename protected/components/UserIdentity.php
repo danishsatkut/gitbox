fd868e743bd832db6c03771dbf8a9732c4fca54c
Vivek Rathod <vivek_rats100@yahoo.co.in>
2012-04-23 01:45:29 +0530

    Added new state in User Indentity to be stored in sessions.

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -20,19 +20,27 @@ class UserIdentity extends CUserIdentity
 	 */
 	public function authenticate()
 	{
-        // where $this->username is declared in parent CUserIdentity
-        $user = User::model()->findByAttributes(array("email"=>$this->username));
+            // where $this->username is declared in parent CUserIdentity
+            $user = User::model()->findByAttributes(array("email"=>$this->username));
         
-		if($user === null)  // user does not exists
-			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		else if(!$user->validatePassword($this->password))    // password does not match
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
-		else {
-            // Store id and username in sessions
-            $this->_id = $user->id;
-            $this->username = $user->username;
-			$this->errorCode=self::ERROR_NONE;
+            if($user === null)  // user does not exists
+                    $this->errorCode=self::ERROR_USERNAME_INVALID;
+            else if(!$user->validatePassword($this->password))    // password does not match
+                    $this->errorCode=self::ERROR_PASSWORD_INVALID;
+            else {
+                // Store id and username in sessions
+                $this->_id = $user->id;
+                $this->username = $user->username;
+                $this->errorCode=self::ERROR_NONE;
+                
+                /*------------------ IMPORTANT --------------------------------
+                 * please remove the following code when avatar is implemented
+                 */
+                $user->avatar = Yii::app()->baseUrl . "/images/user.jpeg";
+                /* ---------------------------------------------------------- */
+                $this->setState('avatar', $user->avatar);
+                $this->setState('email', $user->email);
+            }
+            return !$this->errorCode;
         }
-        return !$this->errorCode;
-    }
 }
