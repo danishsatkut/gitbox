b5f2b22f04d1976894aa7b1228919f915341b69c
Vivek Rathod <vivek_rats100@yahoo.co.in>
2012-04-23 03:43:37 +0530

    Modified the main layout to implement the UI.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,41 +1,84 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="language" content="en" />
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="GITBox - File Hosting, Sharing and Forum Service">
+        <meta name="author" content="Bhavesh Prajapati, Vivek Rathod, Danish Satkut">
 
-	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
-	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
-	<![endif]-->
+        <!-- Le styles -->
+        <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.css" rel="stylesheet">
+        <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/gitbox.css" rel="stylesheet">
+        <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap-responsive.css" rel="stylesheet">
+        <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
-
+        <!-- Le fav and touch icons -->
+        <link rel="shortcut icon" href="<?php echo Yii::app()->request->baseUrl; ?>/ico/favicon.ico">
+        
 	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-</head>
+    </head>
 
 <body>
 
-<div class="container" id="page">
-
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
-
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
+    <?php 
+        $navbar = $this->beginWidget('ext.ziix.widgets.CNavbar', array(
+            'brand'=>array('label'=>'GITBox', 'url'=>array('site/index')),
+            'responsive'=>true,
+        ));
+    ?>
+    
+    <?php
+        $this->widget('ext.ziix.widgets.CNav', array(
+            'items'=>array(
+                array('label'=>'Files', 'url'=>array('site/index'), 'visible'=>!Yii::app()->user->isGuest, 
+                    'icon'=>array('class'=>'icon-inbox icon-white'),),
+                array('label'=>'Forum', 'url'=>Yii::app()->baseUrl . '/forum',
+                    'icon'=>array('class'=>'icon-group-white')),
+                array('label'=>'Contacts', 'url'=>array('contacts/index'), 'visible'=>!Yii::app()->user->isGuest,
+                    'icon'=>array('class'=>'icon-contact-white')),
+                array('label'=>'About', 'url'=>array('site/page', 'view'=>'about'),
+                    'icon'=>array('class'=>'icon-info-sign icon-white')),
+                array('label'=>'Help', 'url'=>array('site/page', 'view'=>'help'),
+                    'icon'=>array('class'=>'icon-flag icon-white')),
+            ),
+            'htmlOptions'=>array('class'=>'nav'),
+        ));
+    ?>
+    
+    <?php
+        $this->widget('ext.ziix.widgets.CNav', array(
+            'items'=>array(
+                array('label'=>Yii::app()->user->name, 'url'=>array('#user'), 'visible'=>!Yii::app()->user->isGuest, 
+                        'icon'=>array(
+                            'class'=>'icon-user-avatar',
+                            'path'=>Yii::app()->user->getState('avatar'),
+                            ),
+                    
+                    'linkOptions'=>array('class'=>'dropdown-toggle', 'data-toggle'=>'dropdown'),
+                    'itemOptions'=>array('class'=>'dropdown'),
+                    'caret'=>true,
+                    'items'=>array(
+                        array('label'=>'Profile', 'url'=>array('user/profile'),
+                            'icon'=>array('class'=>'icon-user')),
+                        array('label'=>'Settings', 'url'=>array('user/settings'),
+                            'icon'=>array('class'=>'icon-cog')),
+                        array('label'=>'Logout', 'url'=>array('site/logout'),
+                            'icon'=>array('class'=>'icon-off')),
+                    ),
+                    'submenuOptions'=>array('class'=>'dropdown-menu'),
+                    
+                )
+            ),
+            'htmlOptions'=>array('class'=>'nav pull-right'),    // Set options on outer ul
+        ));
+    ?>
+    
+    <?php $this->endWidget(); ?>
+    
+    
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 			'links'=>$this->breadcrumbs,
@@ -51,6 +94,22 @@
 	</div><!-- footer -->
 
 </div><!-- page -->
-
+    
+    <!-- Le javascript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-transition.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-alert.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-modal.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-dropdown.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-scrollspy.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-tab.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-tooltip.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-popover.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-button.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-collapse.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-carousel.js"></script>
+        <script src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-typeahead.js"></script>
 </body>
 </html>
