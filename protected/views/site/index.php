f74a8834181ffca46aa2fe12f87bcba868ad559d
Vivek Rathod <vivek_rats100@yahoo.co.in>
2012-04-23 11:19:22 +0530

    Added the markup from design for folder

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,17 +1,145 @@
-<?php $this->pageTitle=Yii::app()->name; ?>
+<div class="row">
+    <div class="span9 tabbable"><!-- Tabs for switching between files and comments  -->
+        <ul class="nav nav-tabs">
+            <li class="active"><a href="#1" data-toggle="tab"><i class="icon-folder-open" style="width: 16px"></i> Files and Folders</a></li>
+            <li><a href="#2" data-toggle="tab"><i class="icon-comment"></i> Comments</a></li>
+            <li><a href="#3" data-toggle="tab">Search Results</a></li>
+        </ul>
+        <div class="tab-content minimum-height">
+            <div class="tab-pane active" id="1"><!-- Files tab -->
+                <ul id="clip-list" class="unstyled">
+                    <?php if($user->virtualfolderCount > 0): ?>
+                        <?php foreach($user->virtualfolders as $virtualFolder): ?>
+                            <!-- Display each folder accordingly -->
+                            <li class="clip">
+                                <i class="icon-clip <?php echo $virtualFolder->folder->isShared() ? 'folder-shared' : 'folder-generic'; ?>"></i>
+                                <div class="clip-info">
+                                    <div class="clip-file">
+                                        <span class="clip-file-name"><?php echo $virtualFolder->folder->folderName; ?></span>
 
-<p>Welcome, <?php echo Yii::app()->user->name; ?></p>
+                                        <div class="btn-toolbar clip-buttons pull-right">
+                                            <a class="btn btn-primary btn-mini" href="#share">
+                                                <i class="icon-share"></i> Share
+                                            </a>
+                                            <div class="btn-group ">
+                                                <a class="btn btn-mini dropdown-toggle" data-toggle="dropdown" href="#">
+                                                    Options
+                                                    <span class="caret"></span>
+                                                </a>
+                                                <ul class="dropdown-menu">
+                                                    <li><a href="#rename">Rename</a></li>
+                                                    <li><a href="#move">Move</a></li>
+                                                    <li><a href="#copy">Copy</a></li>
+                                                    <li><a href="#comment">Comment</a></li>
+                                                </ul>
+                                            </div>
+                                            <button class="btn btn-mini btn-danger file-delete">
+                                                <i class="icon-trash"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div class="clip-file-description">
+                                        <span class="clip-file-status"><?php echo $virtualFolder->folder->lastActionLabel(); ?></span>
+                                        <span class="clip-file-date"><?php echo $virtualFolder->folder->lastActionDate(); ?></span> by
+                                        <span class="clip-file-user"><a href="<?php echo $this->createUrl('user/view', array('id'=>Yii::app()->user->id)); ?>"><?php echo $virtualFolder->folder->lastActionBy(); ?></a></span> - 
+                                        <span class="clip-file-count"><i class="icon-file"></i><?php echo $virtualFolder->folder->filesCount; ?></span>
+                                        <span class="clip-file-comments"><i class="icon-comment"></i> 0</span>
+                                        <?php if($virtualFolder->folder->isShared()): ?>
+                                        <span class="clip-file-collaborators"><i class="icon-user"></i> </span>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+                            </li>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                    
+                    
+                    <li class="clip">
+                        <i class="icon-clip thumbnail"><img src="img/44x44.gif" alt="Image Snapshot"></i>
+                        <div class="clip-info">
+                            <div class="clip-file">
+                                <span class="clip-file-name">Web Design.gif</span>
+                                <div class="btn-toolbar clip-buttons pull-right">
+                                <a class="btn btn-primary btn-mini" href="#share">
+                                    <i class="icon-share"></i> Share
+                                </a>
+                                <div class="btn-group ">
+                                    <a class="btn btn-mini dropdown-toggle" data-toggle="dropdown" href="#">
+                                        Options
+                                        <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#rename">Rename</a></li>
+                                        <li><a href="#move">Move</a></li>
+                                        <li><a href="#copy">Copy</a></li>
+                                        <li><a href="#comment">Comment</a></li>
+                                    </ul>
+                                </div>
+                                <button class="btn btn-mini btn-danger file-delete">
+                                    <i class="icon-trash"></i>
+                                </button>
+                            </div>
+                            </div>
+                            <div class="clip-file-description">This is a text file</div>
+                        </div>
+                    </li>
+                    <li class="clip">
+                        <i class="icon-clip file-docx"></i>
+                        <div class="clip-info">
+                            <div class="clip-file">
+                                <span class="clip-file-name">Word.docx</span>
+                                <div class="btn-toolbar clip-buttons pull-right">
+                                    <a class="btn btn-primary btn-mini" href="#share">
+                                        <i class="icon-share"></i> Share
+                                    </a>
+                                    <div class="btn-group ">
+                                        <a class="btn btn-mini dropdown-toggle" data-toggle="dropdown" href="#">
+                                            Options
+                                            <span class="caret"></span>
+                                        </a>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#rename">Rename</a></li>
+                                            <li><a href="#move">Move</a></li>
+                                            <li><a href="#copy">Copy</a></li>
+                                            <li><a href="#comment">Comment</a></li>
+                                        </ul>
+                                    </div>
+                                    <button class="btn btn-mini btn-danger file-delete">
+                                        <i class="icon-trash"></i>
+                                    </button>
+                                </div>
+                            </div>
+                            <div class="clip-file-description">This is a docx file.</div>
+                        </div>
+                    </li>
+                </ul>
 
-<div>
-    <span><?php echo CHtml::link('Create new folder', $this->createUrl('folder/create')); ?></span>
-    <span><?php echo CHtml::link('Upload File', $this->createUrl('file/upload')); ?></span>
+            </div>
+            <div class="tab-pane" id="2"><!-- Comments tab -->
+
+                <ul id="comment-list" class="unstyled">
+                    <li class="clip">
+                        <i class="icon-clip user-avatar"></i>
+                        <div class="clip-info">
+                            <div class="clip-file">
+                                <span class="clip-file-name">Vivek Rathod</span>
+                            </div>
+                            <div class="btn-toolbar clip-buttons pull-right">
+                                    <a class="btn btn-mini" href="#edit">
+                                        <i class="icon-share"></i> Edit
+                                    </a>
+                                    <button class="btn btn-mini btn-danger file-delete">
+                                        <i class="icon-trash"></i>
+                                    </button>
+                                </div>
+                            <div class="clip-file-description">This rar file contains all the program</div>
+                        </div>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
 </div>
-
-<div>
-    <span>Your folders</span>
-</div>
-
-<?php echo "There are " . count($user->virtualfolders) . " folders in your box"; ?>
 
 <?php foreach($user->virtualfolders as $virtualFolder): ?>
     <div>
